9f24ff907a92dda11260804fa8752cf6adccf51f
Ivan Alekseev <iv.s.alekseev@tinkoff.ru>
2024-07-30 21:39:26 +0300

    first commit

---
+++ src/main/java/com/edu/RmiClient.java
@@ -0,0 +1,23 @@
+package com.edu;
+
+import com.edu.dto.BookRMI;
+import com.edu.rmi.BookRemoteService;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.List;
+
+public class RmiClient {
+    public static void main(String[] args) {
+        try {
+            Registry registry = LocateRegistry.getRegistry("localhost", 1099);
+            BookRemoteService bookService = (BookRemoteService) registry.lookup("BookRemoteService");
+
+            // Пример использования RMI сервиса
+            List<BookRMI> books = bookService.getAllBooks(0, 10);
+            books.forEach(book -> System.out.println(book.getId()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
